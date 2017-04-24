7a5d0015d5b40c46b78d99b673205bf7bd40e45e
gschwarz11 <bardavelidze11@mail.ru>
2017-04-24 14:27:04 +0200

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,1 +1,19 @@
+#Funktion zum Starten
+def startEngine():
+  
+  return;
 
+#Funktion zum Stoppen
+def stopEngine():
+  
+  return;
+
+#Funktion zur Beschleunigung
+def accelerateEngine():
+  
+  return;
+
+#Funktion zur Abbiegen
+def changeDirEngine():
+  
+  return;
