42b81d9851b564bdc3f867bd5c1dd7fcd4eb08b4
gschwarz11 <bardavelidze11@mail.ru>
2017-04-24 14:32:13 +0200

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,19 +1,20 @@
-#Funktion zum Starten
+#Function to start the engine
 def startEngine():
   
-  return;
+	return;
 
-#Funktion zum Stoppen
+#Function to stop the engine
 def stopEngine():
   
-  return;
+	return;
 
-#Funktion zur Beschleunigung
+#Function for acceleration
 def accelerateEngine():
   
-  return;
+	return;
 
-#Funktion zur Abbiegen
-def changeDirEngine():
+#Function for turning
+#Accepts an angle of the tires (-90 until 90). Use negative to change to left or positive to change to right
+def changeDirEngine(angle):
   
-  return;
+	return;
