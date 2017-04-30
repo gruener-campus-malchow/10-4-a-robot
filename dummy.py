b99cce2519fea33e2ed6c641847bda0b40fea628
gschwarz11 <bardavelidze11@mail.ru>
2017-05-01 00:50:01 +0200

    Beschleunigung gemappt

--- dummy.py
+++ dummy.py
@@ -3,6 +3,12 @@ def mainEngineClass():
 
 	#Initialisation Function
 	def __init__():
+	
+		global DC_FW_MAX = 7.0		#Maximal Duty_Cycle-Value for moving forward
+		global DC_FW_MIN = 5.1		#Minimal Duty_Cycle-Value for moving forward
+		global DC_STOP   = 5.0		#Duty_Cycle-Value for stopping the motors
+		global DC_BW_MIN = 4.9		#Minimal Duty_Cycle-Value for moving backwards
+		global DC_BW_MIN = 3.0		#Maximal Duty_Cycle-Value for moving backwards
 
 		return;
 
@@ -57,25 +63,33 @@ def mainEngineClass():
 		return;
 
 	#Function for acceleration
-	#Acc is a number from -100 to 100 for accelerating
+	#Acc is a number from -1 to 1 for accelerating
 	def accelerateEngine(acc):
 
 		if turnedOn:
+		
+			toImportArray = ['math']
+			importIt(toImportArray)
 
-			if acc > 0:
-				acceleration = [5,7]
-			elif acc < 0:
-				acceleration = [3,5]
-				acc = acc * (-1)
-			elif acc == 0:
-				stopEngine()
-
-			thruster = ((acceleration[0]/acceleration[1])*acc)
-
+			accelerationValue = float(acc)
+			if (value > 0 and value <= 1):
+				return remap(value, 0, 1, DC_FW_MIN, DC_FW_MAX)
+			elif (value < 0 and value >= -1):
+				return remap(math.fabs(value), 1, 0, DC_BW_MAX, DC_BW_MIN)
+			elif (value == 0):
+				return DC_STOP
+			else:
+				print "WARNING: Please enter a value between -1 and 1"
+			
 		else:
 			print "WARNING: Motor is offline"
 
 		return;
+		
+	#Function for remapping
+	#Map series of numbers with other ones
+	def remap(value, leftMin, leftMax, rightMin, rightMax):
+		return rightMin + ((float(value - leftMin) / float(leftMax - leftMin)) * (rightMax - rightMin))
 
 	#Function for turning
 	#Accepts an angle of the tires (-90 until 90). Use negative to change to left or positive to change to right
